827f4028eebb326e3f4891f217d6635e197ae193
Kristen Bartel <kristenbartel@gmail.com>
2021-12-14 14:06:57 -0600

    completed arrays

--- js101-arrays.js
+++ js101-arrays.js
@@ -1,29 +1,42 @@
-const friendInfo = {
-    "name": "Alan Turing",
-    "cell": "6017576",
-    "birthday": "June 23",
+
+const theNames = ["Kristen", "Bartel", "javaScript"];
+const today = [12, 14, 21];
+// const intro = theNames.length;
+
+// function printArr(theName, datesFree) {
+//     for (let i = 0; i < theNames.length; i++) {
+//         console.log(theNames[i]);
+//     }
+// }
+
+
+// console.log(theNames.join('! '));
+// console.log(theNames[0] + theNames[1] + theNames[2]);
+// console.log(`hello my name is ${theNames.length}`);
+
+function greeting() {
+    //theNames.push("yay!"); work on this one later
+    today[4] = 30
+// couple ways to add/push into an array above
+    theNames.splice(2, 0, "I am learning");
+    theNames.splice(3, 2, "HTML/CSS");
+// these are ways to insert items at a specific index location
+// the first value is the start point, the second value is the item you want to delete (if any)
+    return (`Hello my name is ${theNames.join(' ')}. The date is ${today.join('-')}!`);
 }
 
-// Object values can be any valid JavaScript type!
+// console.log(greeting());
+// //  console.log(theNames);
+// console.log(theNames[3]);
+// console.log(theNames.length);
 
 
-const superhero = {
-    name: "Wonder Woman",
-    alias: "Diana Prince",
-    bracelets: 2,
-    lassos: 1
+const langs = ["HTML", "CSS", "JavaScript", "React", "Bananas"]
+function introduction() {
+    
+    langs.pop();
+    langs.push("Node");
+    return (`I am learning ${langs.join(', ')}!`);
 }
-
-// There are two ways to retrieve data from an object
-// [<key name with quotes>] retrieves the value for a key using the index syntax
-// .<key name> retrieves the value for a key using the dot syntax
-
-const superhero = {
-    name: "Wonder Woman",
-    alias: "Diana Prince",
-    bracelets: 2,
-    lassos: 1
-}
-
-hero_name = superhero["name"]
-number_of_lassos = superhero.lassos
+console.log(introduction(langs));
+// how to add the word and in this sentence using this syntax
