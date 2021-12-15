86d91b16081d1d50c6e0d89e68bf0ddc40362ae2
Kristen Bartel <kristenbartel@gmail.com>
2021-12-14 20:35:34 -0600

    completed js-101-exercises

---
+++ js101-conditionals.js
@@ -0,0 +1,25 @@
+myFriendsSchedule = {
+    sunday: true,
+    monday: false,
+    tuesday: false,
+    wednesday: true,
+    thursday: false,
+    friday: true,
+    saturday: true
+}
+myMoneySchedule = {
+    sunday: 10,
+    monday: 9,
+    tuesday: 8,
+    wednesday: 12,
+    thursday: 15,
+    friday: 20,
+    saturday: 2
+}
+
+function canWeGoToTheMovies() {
+    if (myFriendsSchedule, myMoneySchedule) {
+        
+    }
+}
+caWeGoToTheMovies(sunday)
